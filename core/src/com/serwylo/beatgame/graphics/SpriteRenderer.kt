268b6abd4c18cf404e76406bd2e46dbf4db10f3a
Cody Wallace <Cody@codex419.me>
2026-03-06 04:14:51 -0500

    Delete all files

--- core/src/com/serwylo/beatgame/graphics/SpriteRenderer.kt
+++
@@ -1,7 +0,0 @@
-package com.serwylo.beatgame.graphics
-
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
-
-interface SpriteRenderer {
-    fun render(batch: SpriteBatch)
-}
--- song-extract/src/com/serwylo/beatgame/bin/SongExtractLauncher.kt
+++
@@ -1,9 +0,0 @@
-package com.serwylo.beatgame.bin
-
-import com.badlogic.gdx.backends.headless.HeadlessApplication
-import com.serwylo.beatgame.desktop.SongExtract
-
-fun main(arg: Array<String>) {
-    HeadlessApplication(SongExtract(arg))
-}
-
